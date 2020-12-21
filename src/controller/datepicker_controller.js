03bb5b7968619d43d0f80e05c9fc54ec07cda22b
jehong <jehong@student.42seoul.com>
2020-12-21 12:41:03 +0900

    feat: test lookup button

--- src/controller/datepicker_controller.js
+++ src/controller/datepicker_controller.js
@@ -10,7 +10,7 @@ export default function DatepickerController() {
       const result = getDatepickerInputValues();
 
       if (isValidInput(result)) {
-        //crawler();
+        console.log(result);
       }
 
     })
